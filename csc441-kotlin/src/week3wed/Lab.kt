dab058d6072ac01146072edb97c7ed3e215dfba7
SeanPSimcox <massvoidness@gmail.com>
2026-09-11 13:05:29 -0400

    Week 3 Wed lab

--- csc441-kotlin/src/week3wed/Lab.kt
+++ csc441-kotlin/src/week3wed/Lab.kt
@@ -1,2 +1,26 @@
 package week3wed
 
+val name = "Sean"
+val major = "CSC"
+val favFood = "Taco's"
+val String = "Words"
+val Int = 69
+val Double = 3.14
+val Boolean = false
+
+var coursesTaken = 60
+var coursesTakenAdd = coursesTaken + 1
+
+fun main() {
+    println(name)
+    println(major)
+    println(favFood)
+    println("Courses taken: $coursesTaken")
+    println("Course taken after this one: ${coursesTaken + 1}")
+    println("$name is studying ${major}.")
+    println("$name has ${name.length} letters.")
+    print("Where are you from? ")
+    val location = readlnOrNull()
+    println("${location} is a good place.")
+
+}
