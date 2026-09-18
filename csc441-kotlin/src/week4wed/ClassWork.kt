938248bb4d76e425a6588f3674491a4a2e9c6139
SeanPSimcox <massvoidness@gmail.com>
2026-09-18 11:25:46 -0400

    Updating in class excercise wed 4 classwork

--- csc441-kotlin/src/week4wed/ClassWork.kt
+++ csc441-kotlin/src/week4wed/ClassWork.kt
@@ -2,30 +2,32 @@ package week4wed
 
 fun main(){
     println("--- Step 1: function returning nothing ---")
-    todaysExcercise()
+    todaysExercise()
 
-//    println("--- Step 2: function returning a value ---")
-//    println(todaysExcercise1())
-//
-//    println("--- Step 3: a parameter ---")
-//
-//    println(todaysExcercise2(9))
-//    println("--- Step 4: the short form ---")
-//
-//    println("--- Step 5: function returning nothing ---")
-//    println(todaysExcercise4())
-//    println(todaysExcercise4(dayNumber))
-//
-//    println("---Step 6: two parameters, and naming them ---")
-//    println(todaysExcercise5(dayNumber = 2, name= "Dzemal"))
-//    println(todaysExercise5(name ="Sam"))
+    println("--- Step 2: function returning a value ---")
+    println("Today I'll do ${todaysExercise1()}")
+
+    println("--- Step 3: a parameter ---")
+    println(todaysExercise2(3))
+    println(todaysExercise2(9))
+
+    println("--- Step 4: short form ---")
+    println(todaysExercise3(3))
+
+    println("--- Step 5: default value ---")
+    println(todaysExercise4())
+    println(todaysExercise4(2))
+
+    println("--- Step 6: named arguments ---")
+    println(todaysExercise5(dayNumber = 2, name= "Sean"))
+    println(todaysExercise5(name = "Sam"))
 }
 
-fun todaysExcercise(){
+fun todaysExercise(){
     println("Push-ups")
 }
 
-fun todaysExercise1(): String{
+fun todaysExercise1(): String {
     return "Push-ups"
 }
 
@@ -40,19 +42,26 @@ fun todaysExercise2(dayNumber: Int): String {
     }
 }
 
-fun todaysExcercise3(dayNumber: Int): String = when(dayNumber) {
+fun todaysExercise3(dayNumber: Int): String = when (dayNumber) {
     1 -> "Push-ups"
     2 -> "Running"
     3 -> "Swimming"
     4 -> "Cycling"
     5 -> "Gym"
     else -> "Rest day"
-
 }
-fun todaysExercise4(dayNumber: Int): String = when(dayNumber) {}
+
+fun todaysExercise4(daynumber: Int = 1): String = when (daynumber) {
+    1 -> "Push-ups"
+    2 -> "Running"
+    3 -> "Swimming"
+    4 -> "Cycling"
+    5 -> "Gym"
+    else -> "Rest day"
+}
 
 fun todaysExercise5(dayNumber: Int = 1, name: String = "you"): String {
-    val excercise = when(dayNumber) {
+    val exercise = when (dayNumber) {
         1 -> "Push-ups"
         2 -> "Running"
         3 -> "Swimming"
@@ -60,5 +69,5 @@ fun todaysExercise5(dayNumber: Int = 1, name: String = "you"): String {
         5 -> "Gym"
         else -> "Rest day"
     }
-    return "$name: $excercise"
+    return "$name: $exercise"
 }
